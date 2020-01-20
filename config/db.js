c99aad002a1e47fcfceb62b2e1168011681fcf3b
Daniel <Daniellagowskii@gmail.com>
2020-01-20 12:40:07 +0100

    update db connection options

--- config/db.js
+++ config/db.js
@@ -10,7 +10,8 @@ const connectDB = async() => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useFindAndModify: false
         });
 
         console.log('Database connected'.magenta);
