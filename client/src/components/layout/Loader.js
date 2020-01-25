7bc6525da0bef2bb2405b2eb75688974c6329f69
Daniel <Daniellagowskii@gmail.com>
2020-01-25 12:45:21 +0100

    Add loaded component

---
+++ client/src/components/layout/Loader.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import loading from "./loading.gif";
+
+const Loader = props => {
+  return (
+    <div style={loaderStyles}>
+      <img src={loading} alt="Loader gif" style={{ width: "100px" }} />
+    </div>
+  );
+};
+
+const loaderStyles = {
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  minHeight: "85vh",
+  padding: "1rem"
+};
+
+export default Loader;
